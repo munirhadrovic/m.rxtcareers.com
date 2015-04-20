880c9ebd57e14b0964027aa9276607189b56efec
anes89cok <anes89cok@gmail.com>
2015-04-20 15:47:49 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,35 @@
+<?php
+$adresa[0]="1323 Hwy 80 East Demopolis, AL 36732"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[0]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[0]="Taco Bell";
+$telephone[0]=3343414728;// Broj koji se poziva na dugme CALL US
+
+$adresa[1]="1003 Gest Street Cincinnati, OH 45203"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[1]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[1]="Taco Bell / Pizza Hut";
+$telephone[1]=5137296424;// Broj koji se poziva na dugme CALL US
+
+$adresa[2]="1230 E. Main Carbondale, IL 62901"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[2]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[2]="Long John Silvers";
+$telephone[2]=6183194696;// Broj koji se poziva na dugme CALL US
+
+$adresa[3]="313 Hwy 82 E Winona, MS 68967"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[3]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[3]="Kentucky Fried Chicken";
+$telephone[3]=6628454501;// Broj koji se poziva na dugme CALL US
+
+$adresa[4]="826 S. Third St. Memphis, TN 38106"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[4]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[4]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[4]="Taco Bell / Long John Silvers";
+$telephone[4]=9018819673;// Broj koji se poziva na dugme CALL US
+
+$companynumber=5;
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
